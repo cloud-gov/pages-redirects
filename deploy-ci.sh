b50cff5657d8290f004670a3d1f3a046fdae7702
James Seppi <james.seppi@gsa.gov>
2017-05-19 14:06:24 -0500

    test deploying to sandbox

--- deploy-ci.sh
+++ deploy-ci.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # CF_USER and CF_PASS are defined as private Environment Variables
-# in the Travis web UI: https://travis-ci.org/18F/pages-redirects/settings
+# in the Circle web UI: https://circleci.com/gh/18F/pages-redirects/edit#env-vars
 
 set -eu
 
 API="https://api.fr.cloud.gov"
-ORG="gsa-18f-federalist"
-SPACE="redirects"
-APP_NAME="pages-redirects"
+# ORG="gsa-18f-federalist"
+# SPACE="redirects"
+ORG="sandbox-gsa"
+SPACE="james.seppi"
+APP_NAME="pages-redirects-testing"
 MANIFEST="out/manifest-prod.yml"
 
 cf login -a $API -u $CF_USER -p $CF_PASS -o $ORG -s $SPACE
